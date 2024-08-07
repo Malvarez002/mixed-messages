53e05ea653e9e6cdfed58981a62448f3641b8d8b
Manuel Alvarez <malvarezg002@gmail.com>
2024-08-07 18:04:15 -0500

    Add Quotes

---
+++ public/quotes.js
@@ -0,0 +1,39 @@
+export const quotes = [
+    {
+        quote: "Life is about making an impact, not making an income. ", author: "- Kevin Kruse"
+    },
+    {
+        quote: "Whatever the mind of man can conceive and believe, it can achieve. ", author: "–Napoleon Hill"
+    },
+    {
+        quote: "Strive not to be a success, but rather to be of value. ", author: "–Albert Einstein"
+    },
+    {
+        quote: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  ", author: "–Robert Frost"
+    },
+    {
+        quote: "I attribute my success to this: I never gave or took any excuse. ", author: "–Florence Nightingale"
+    },
+    {
+        quote: "You miss 100 % of the shots you don’t take. ", author: "–Wayne Gretzky"
+    },
+    {
+        quote: "The most difficult thing is the decision to act, the rest is merely tenacity. ", author: "–Amelia Earhart"
+    },
+    {
+        quote: "Every strike brings me closer to the next home run. ", author: "–Babe Ruth"
+    },
+    {
+        quote: "Definiteness of purpose is the starting point of all achievement. ", author: "–W.Clement Stone"
+    },
+    {
+        quote: "Life isn't about getting and having, it's about giving and being. ", author: "–Kevin Kruse"
+    },
+    {
+        quote: "Life is what happens to you while you’re busy making other plans. ", author: " –John Lennon"
+    },
+    {
+        quote: "We become what we think about. ", author: "–Earl Nightingale"
+    }]
+
+
